82029fbe3a057e63edd716215eef8209cd148ea2
Joon Ki Hong <jhong@sapient.com>
2016-09-21 15:03:58 -0400

    Initial Commit

---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Joon Hong on 9/21/16.
+//  Copyright © 2016 JoonKiHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Instagram
+//
+//  Created by Joon Hong on 9/21/16.
+//  Copyright © 2016 JoonKiHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
